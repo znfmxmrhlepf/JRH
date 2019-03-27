753af920d576a1cd90d1e3b9ecdc7f4a3268a904
znfmxmrhlepf <znfmxmrhlepf@gmail.com>
2019-03-27 22:43:49 +0900

    .

---
+++ code/CNN.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+model = tf.keras.Sequential([
+    layers.BatchNormalization(),
+    layers.Conv2D(32, (16, 16), input_shape=(100, 100, 1), activation='relu'),
+    layers.Conv2D(64, (12, 12), activation='relu'),
+    layers.Conv2D(64, (10, 10), activation='relu'),
+    layers.Conv2D(128, (8, 8), activation='relu'),
+    layers.Conv2D(128, (5, 5), activation='relu'),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(19, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
