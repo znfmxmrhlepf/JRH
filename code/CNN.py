40b368d5b6a63bd51d9630a80470c2b0e313eda4
ishs <znfmxmrhlepf@gmail.com>
2019-04-03 20:02:04 +0900

    .

--- code/CNN.py
+++ code/CNN.py
@@ -1,8 +1,11 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 import os
+import tool
 
-os.chdir('..')
+tf.enable_eager_execution()
+
 f = open('Images/list.txt')
 class_names = list(map(lambda x: x[:-1], f.readlines()))
 class_names.sort()
@@ -10,8 +13,8 @@ class_names.sort()
 data_test, data_train = tool.load_data()
 
 model = tf.keras.Sequential([
-    layers.BatchNormalization(),
-    layers.Conv2D(32, (7, 7), input_shape=(60, 60, 1), activation='relu'),
+    # layers.BatchNormalization(),
+    layers.Conv2D(32, (7, 7), input_shape=(60, 60, 3), activation='relu'),
     layers.Conv2D(64, (7, 7), activation='relu'),
     layers.Conv2D(64, (5, 5), activation='relu'),
     layers.Conv2D(128, (5, 5), activation='relu'),
@@ -23,7 +26,9 @@ model = tf.keras.Sequential([
     layers.Dense(19, activation='softmax')
 ])
 
+model.compile(tf.train.AdamOptimizer(), tf.losses.softmax_cross_entropy)
 def loss(model, x, y):
+    print(x.shape)
     y_ = model(x)
     return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
 
@@ -31,8 +36,37 @@ def grad(model, inputs, targets):
     with tf.GradientTape() as tape:
         lossVal = loss(model, inputs, targets)
     return lossVal, tape.gradient(lossVal, model.trainable_variables)
-    
 
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+global_step = tf.Variable(0)
 
-for i in range(100):
-    model.fit(data_train.get_batch())
+from tensorflow import contrib
+tfe = contrib.eager
+
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs = 200
+
+for epoch in range(num_epochs):
+    epoch_loss_avg = tfe.metrics.Mean()
+    epoch_accuracy = tfe.metrics.Accuracy()
+
+    tmp1, tmp2 = data_train.get_batch(40)
+    tmp1, tmp2 = tf.cast(tmp1, tf.float32), tf.cast(tmp2, tf.float32)
+    for i in range(40):
+        loss_value, grads = grad(model, np.expand_dims(tmp1[i], axis=0), np.expand_dims(tmp2[i], axis=0))
+        optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step)
+
+    epoch_loss_avg(loss_value)
+    epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+
+    train_loss_results.append(epoch_loss_avg.result())
+    train_accuracy_results.append(epoch_accuracy.result())
+
+    if epoch % 10 == 0:
+        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch, epoch_loss_avg.result(), epoch_accuracy.result()))
+
+for i in range(10):
+    tmp = data_train.get_batch(1000)
+    model.fit(tmp[0], tmp[1])
--- code/tool.py
+++ code/tool.py
@@ -9,18 +9,18 @@ random.seed(time.time())
 
 def get_images(paths):
     images = np.zeros((len(paths), 60, 60, 3))
-    labels = np.zeros((len(paths), 19))
+    labels = np.zeros(len(paths))
 
     for i, path in enumerate(paths):
-        images[i, :, :, :] = cv2.resize(cv2.imread(path), None, fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA)
-        print(path[:path.find('_')])
+        img = cv2.imread(path)
+        images[i, :, :, :] = cv2.resize(img, (60, 60), interpolation=cv2.INTER_AREA)
+        path = os.path.basename(path) 
         labels[i] = int(path[:path.find('_')])
 
-    return (images, labels)
+    return images, labels
 
 def load_data():
 
-    os.chdir('..')
     f = open('Images/list.txt')
     classes = list(map(lambda x:  x[:-1], f.readlines()))
     classes.sort()
@@ -29,7 +29,8 @@ def load_data():
     for classf in classes:
         paths += glob.glob('Images/' + classf + '/*')
 
-    imagePaths = [os.path.basename(x) for x in paths]
+    imagePaths = paths
+    # imagePaths = [os.path.basename(x) for x in paths]
     random.shuffle(imagePaths)
     
     data_test = data(imagePaths[:500])
@@ -43,7 +44,9 @@ class data():
 
     def get_batch(self, size):
         random.shuffle(self.paths)
-        return get_images(self.paths[:size])
+        x, y = get_images(self.paths[:size])
+        return x, y
 
 if __name__ == '__main__':
-    load_data()
+    data_test, _ = load_data()
+    x, y = data_test.get_batch(40)
